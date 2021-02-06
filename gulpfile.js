6ceefd4f7cec46f1d405e24158c76f5dff48ead9
Marcelo <marcelo.tld.snchez@gmail.com>
2021-02-06 12:42:42 -0300

    Se ha modificado la estructura de las tareas.

--- gulpfile.js
+++ gulpfile.js
@@ -6,94 +6,29 @@ const minify = require('gulp-clean-css');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-// JS plugin
-const rollup = require('gulp-better-rollup')
-	sourcemaps = require('gulp-sourcemaps'),
-	uglify = require('gulp-uglify-es').default,
-	babel = require('rollup-plugin-babel');
-
-
-// Basic config
-const config = {
-	path: {
-		css: {
-			watch: './source/scss/**/*.scss',
-			dest: './dest/css'
-		},
-		js: {
-			watch: './source/js/**/*.js',
-			dest: './dest/js'
-		}
-	}
-}
-
-const css = (pathName) => {
-	return src(pathName)
-	.pipe(sass.sync({outputStyle: 'concat'}).on('error', sass.logError))
-	.pipe(dest(config.path['css'].dest))
+function _CSS(pathName, output) {
+  return src(pathName)
+  .pipe(sass.sync({ outputStyle: 'concat' }).on('error', sass.logError))
+	.pipe(dest(output))
 	.pipe(rename({suffix: '.min'}))
 	.pipe(minify({compatibility: 'ie8'}))
-	.pipe(dest(config.path['css'].dest))
+	.pipe(dest(output))
 }
 
-const printLicense = (name) => {
-	return `/*!
-* ${name}karasu-dev @ v0.1.21
-* Copyright 2020 © Karasu themes
-* Developed by Marcelo (github.com/MarceloTLD)
-* MIT License
-*/`
-}
+// CSS builds
+const css = {}
+css.karasu = () => _CSS('./source/styles/karasu.scss', './dest/css/');
+css.format = () => _CSS('./source/styles/karasu.format.scss', './dest/css/');
+css.component = () => _CSS('./source/styles/karasu.component.scss', './dest/css/');
+css.layout = () => _CSS('./source/styles/karasu.layout.scss', './dest/css/');
+css.utils = () => _CSS('./source/styles/karasu.utils.scss', './dest/css/');
 
-const js = (pathName, fileName, output) => {
-  return src(pathName + fileName)
-    .pipe(sourcemaps.init())
-    .pipe(rollup({
-      plugins: [babel()]
-    }, output))
-    .pipe(sourcemaps.write())
-    .pipe(dest(config.path['js'].dest))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(dest(config.path['js'].dest))
-}
-
-// Build para css
-const cssKarasuBuild = () => css('./source/scss/karasu.scss');
-const cssHelperBuild = () => css('./source/scss/karasu.utils.scss');
-const cssLayoutBuild = () => css('./source/scss/karasu.layout.scss');
-const cssFormBuild = () => css('./source/scss/karasu.form.scss');
-const cssComponentBuild = () => css('./source/scss/karasu.component.scss');
-
-// Build para javascript
-const jsBuild = () => js('./source/js/', 'karasu-dev.js', {name: 'raven', format: 'iife', banner: printLicense('')});
-const jsUtils = () => js('./source/js/utils/', 'utils.js', {name: 'utils', format: 'iife', banner: printLicense('Utils - ')});
-const jsComponent = () => js('./source/js/components/', 'component.js', {name: 'components', format: 'iife', banner: printLicense('Components - ')});
-
-
-// Tareas para los estilos css
-exports.karasu = cssKarasuBuild;
-exports.layout = cssLayoutBuild;
-exports.helper = cssKarasuBuild;
-exports.form = cssFormBuild;
-exports.component = cssComponentBuild;
-exports.cssBuild = series( cssKarasuBuild,  cssLayoutBuild, cssComponentBuild, cssFormBuild);
-
-
-// Tareas para JS
-exports.js = jsBuild;
-exports.jsBuild = series( jsBuild,  jsComponent, jsUtils);
-exports.jsComponent = jsComponent;
-exports.jsUtils = jsUtils;
-
-// Monitorear todas las tareas en tiempo real
-exports.dev = () => {
-	watch([config.path['css'].watch], series(cssLayoutBuild));
-	watch([config.path['css'].watch], series(cssComponentBuild));
-	watch([config.path['css'].watch], series(cssFormBuild));
-	watch([config.path['css'].watch], series(cssHelperBuild));
-	watch([config.path['css'].watch], series(cssKarasuBuild));
-	watch([config.path['js'].watch], series(jsBuild));
-	watch([config.path['js'].watch], series(jsUtils));
-	watch([config.path['js'].watch], series(jsComponent));
+// Tasks
+exports.css = css.karasu;
+exports["css.format"] = css.format;
+exports["css.component"] = css.component;
+exports["css.layout"] = css.layout;
+exports["css.utils"] = css.utils;
+exports["css.watch"] = () => {
+  watch('./source/styles/**/*.scss', series(css.karasu, css.format, css.component, css.layout, css.utils))
 }
