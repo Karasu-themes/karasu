fc46906bb0ecb459c7b492ad3500c9ff020f7c99
Marcelo <marcelo.tld.snchez@gmail.com>
2021-02-09 12:19:50 -0300

    Se ha agregado el módulo utils

---
+++ dest/js/karasu.js
@@ -0,0 +1,14 @@
+/*!
+* karasu - v0.3.0
+* Copyright 2020 © Karasu themes
+* Developed by Marcelo (github.com/MarceloTLD)
+* MIT License
+*/
+(function () {
+	'use strict';
+
+
+
+}());
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoia2FyYXN1LmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OzsifQ==
